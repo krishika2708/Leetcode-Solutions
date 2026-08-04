7f2e76accb30c781865d7b23cab07714cca6cd9b
krishika2708 <krishikasachan3@gmail.com>
2026-08-05 01:10:11 +0530

    Time: 0 ms (100%), Space: 17.8 MB (60.93%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,17 +1,42 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        
-        int maxi=nums[0];
-        
-        for(int i=0;i<nums.size();i++){
-            int p=1;
-                for(int j=i;j<nums.size();j++){
-                     p=p*nums[j];
-                    maxi=max(maxi,p);
-                }
+
+int maxProd = nums[0];
+        int minProd = nums[0];
+        int ans = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] < 0)
+                swap(maxProd, minProd);
+
+            maxProd = max(nums[i], maxProd * nums[i]);
+            minProd = min(nums[i], minProd * nums[i]);
+
+            ans = max(ans, maxProd);
         }
-        return maxi;
-        
+
+        return ans;
+
     }
 };
+
+
+// brute force
+// class Solution {
+// public:
+//     int maxProduct(vector<int>& nums) {
+        
+//         int maxi=nums[0];
+        
+//         for(int i=0;i<nums.size();i++){
+//             int p=1;
+//                 for(int j=i;j<nums.size();j++){
+//                      p=p*nums[j];
+//                     maxi=max(maxi,p);
+//                 }
+//         }
+//         return maxi;
+        
+//     }
+// };
