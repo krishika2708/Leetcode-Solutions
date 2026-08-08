b8171e65e08f990f814e2159e4542f93e525e510
krishika2708 <krishikasachan3@gmail.com>
2026-08-08 18:43:46 +0530

    Time: 15 ms (17.43%), Space: 23 MB (20.18%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -5,13 +5,13 @@ public:
         int n = piles.size();
         int maximum = INT_MIN;
         for (int i = 0; i < n; i++) {
-            maximum= max(maximum, piles[i]);
+            maximum = max(maximum, piles[i]);
         }
         return maximum;
     }
-   long long totalhours(vector<int>& piles, int hourly) {
-      long long total = 0;
-       
+    long long totalhours(vector<int>& piles, int hourly) {
+        long long total = 0;
+
         for (int i = 0; i < piles.size(); i++) {
             total += ceil((double)piles[i] / hourly);
         }
@@ -27,12 +27,12 @@ public:
             long long totalhrs = totalhours(piles, mid);
 
             if (totalhrs <= h) {
-                ;
+
                 high = mid - 1;
             } else {
                 low = mid + 1;
             }
         }
-            return low;
-        }
-    };
+        return low;
+    }
+};
