dc3fe364d8871588b283102f8f9eb757872eac83
krishika2708 <krishikasachan3@gmail.com>
2026-08-13 23:38:41 +0530

    Time: 0 ms (100%), Space: 8.4 MB (92.71%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+  
+       bool isPalindrome(int x) {
+        if(x < 0 || (x % 10 == 0 && x != 0)) return false;
+
+        int rev = 0;
+//half number reverse kr rhe hai overflow avoid krne ke liye
+        while(x > rev) {
+            int rem = x % 10;
+            rev = rev * 10 + rem;
+            x = x / 10;
+        }
+
+        return (x == rev || x == rev / 10);
+    }
+};
