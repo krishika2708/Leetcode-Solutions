985c022ce5a19e64c904cedb20fc2c0ea398afe9
krishika2708 <krishikasachan3@gmail.com>
2026-09-02 15:21:53 +0530

    Time: 0 ms (100%), Space: 8.7 MB (44.65%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    double pow(double x, long long n) {
+        if (n == 0) {
+            return 1;
+        }
+        double half = pow(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half;
+        } else
+            return x * half * half;
+    }
+    double myPow(double x, int n) {
+        long long N = n;
+        if (N < 0) {
+            N = -N;
+            return 1.0 / pow(x, N);
+        }
+
+        return pow(x, N);
+    }
+};
