076a90db2fcc51075652de5005cae7976d6d591d
krishika2708 <krishikasachan3@gmail.com>
2026-09-12 16:04:21 +0530

    Time: 55 ms (9.24%), Space: 49.5 MB (8.44%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+void findcombination(int index,int target,vector<int>arr,vector<vector<int>>&ans,vector<int>&ds){
+    if(index==arr.size()){
+        if(target==0){
+            ans.push_back(ds);
+        }
+        return;
+    }
+    //pick the element
+    if(arr[index]<=target){
+        ds.push_back(arr[index]);
+        findcombination(index,target-arr[index],arr,ans,ds);
+        ds.pop_back();
+    }
+    //not pick
+     findcombination(index+1,target,arr,ans,ds);
+
+}
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int>ds;
+        vector<vector<int>>ans;
+        findcombination(0,target,candidates,ans,ds);
+        return ans;
+    }
+};
+
+
+
